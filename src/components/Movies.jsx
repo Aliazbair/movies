70280786449174e16c890290d6a2546aba6e7780
Aliazbair <ali0995559052@gmail.com>
2023-01-15 23:18:06 +0300

    add search funcationaly

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,31 +1,51 @@
-import { MovieList } from '../data/movies';
-import { Category } from './Category'
+import { useState } from 'react';
+import { MovieList as data } from '../data/movies';
+import { Category } from './Category';
 
 export const Movies = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const handleSearch = (e) => {
+    setSearchInput(e.target.value);
+  };
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
   return (
     <div className='container max-w-7xl mx-auto px-10 py-20 '>
       <Category />
+      {/* input search */}
+      <div className='mt-5'>
+        <input
+          type='search'
+          className='bg-gray-400 w-full px-2 py-3 rounded-sm outline-none placeholder:text-gray-200 text-white'
+          placeholder='Search Here'
+          value={searchInput}
+          onChange={handleSearch}
+        />
+      </div>
       <div className='grid gap-5 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mt-10'>
-        {MovieList.map(({ id, name, description, image, type, category }) => (
-          <div
-            key={id}
-            className='shadow-sm shadow-red-200 p-2 bg-gray-800 rounded'
-          >
-            <img
-              src='https://th.bing.com/th/id/OIP.Vg7gE_hwMujcoiqNbwekmAHaF7?w=228&h=182&c=7&r=0&o=5&pid=1.7'
-              alt=''
-              className='w-full object-cover'
-            />
-            <h2 className='text-gray-200 font-semibold font-mono text-sm md:text-xl '>
-              {name}
-            </h2>
-            <p className='text-gray-400 text-xl truncate'>{description}</p>
-            <div className='flex items-center justify-between flex-wrap '>
-              <span className='text-red-800'>{type}</span>
-              <span className='text-red-800'>{category}</span>
+        {filteredData.map(
+          ({ id, name, description, image, type, category }) => (
+            <div
+              key={id}
+              className='shadow-sm shadow-red-200 p-2 bg-gray-800 rounded'
+            >
+              <img
+                src='https://th.bing.com/th/id/OIP.Vg7gE_hwMujcoiqNbwekmAHaF7?w=228&h=182&c=7&r=0&o=5&pid=1.7'
+                alt=''
+                className='w-full object-cover'
+              />
+              <h2 className='text-gray-200 font-semibold font-mono text-sm md:text-xl '>
+                {name}
+              </h2>
+              <p className='text-gray-400 text-xl truncate'>{description}</p>
+              <div className='flex items-center justify-between flex-wrap '>
+                <span className='text-red-800'>{type}</span>
+                <span className='text-red-800'>{category}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
---
+++ tasks.md
@@ -0,0 +1,18 @@
+# all my tasks my add in the projects
+
+- [ ] add navbar
+- [ ] add search action
+- [ ] add slide bar
+- [ ] add graphql client with query and mutation
+- [ ] add status features
+- [ ] add new movies from youtube
+- [ ] add hover group to movie list
+- [ ] add move slide in category
+- [ ] add rating
+- [ ] add paly video
+- [ ] link with laravel api
+- [ ] use new features of react-router-dom
+- [ ] add authentication
+- [ ] add authorization
+- [ ] add admin manager
+- [ ] filter with category
